76526087e4f0152dea06511f0cd592121030bb76
wangye <2736845454@qq.com>
2024-01-30 14:39:26 +0800

    initialize-Commit

--- src/main.go
+++ src/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World!!!")
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	//if r.Method != http.MethodPost {
+	//	fmt.Println("Method Not Allowed")
+	//	return
+	//}
+	r.ParseForm()
+	fmt.Println("请求参数有", r.Form)
+	fmt.Println("直接", r.Form["username"])
+	fmt.Println("直接", r.Form["password"])
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	fmt.Println(username)
+	fmt.Println(password)
+	//params := r.URL.Query()
+	//name := params.Get("name")
 }
