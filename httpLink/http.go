9f470267b095b3f25302cc9736fa3cf50bfb6684
wangye <2736845454@qq.com>
2024-02-22 10:17:47 +0800

    2024-2-22 beforeChange

--- httpLink/http.go
+++ httpLink/http.go
@@ -12,7 +12,8 @@ import (
 
 func Welcome(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8") //设置请求头
-	fmt.Fprintln(w, "服务器返回的信息<b>你好!!!</b>")                    //打印
+	fmt.Println("welcome!!!")
+	fmt.Fprintln(w, "服务器返回的信息<b>你好!!!</b>") //打印
 }
 
 type User struct {
